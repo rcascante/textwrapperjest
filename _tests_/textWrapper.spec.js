7b9148eeacf8ed8090d2830bbd4cf633ebc33e10
Rita Cascante Makarova <rcascante@MacBook-Pro-de-Rcascate.local>
2018-07-11 13:41:17 +0200

    should break text if there are two spaces

--- _tests_/textWrapper.spec.js
+++ _tests_/textWrapper.spec.js
@@ -7,29 +7,42 @@
 // textWrapper("hello", 2) = > "he\nllo"
 // textWrapper("hello world", 7) = > "hello\nworld"
 // textWrapper("hello world mates", 7) = > "hello\nworld\nmates"
+
 function textWrapper(text, maxWidth) {
     if (maxWidth >= text.length) {
         return text;
     }
     else {
         if (text.includes(' ')) {
-            return "hello\nworld"
+            text = text.replace(' ', '\n');
+            text = text.replace(' ', '\n');
+            console.log(text);
+            return text;
         }
-
-        return 'he\nllo'
+        return "he\nllo"
     }
-}
+};
+
 describe('the test', () => {
     beforeEach((done) => {
         done();
     });
+
     it('should return the same text if it fits', () => {
         expect(textWrapper("hello", 7)).toBe("hello");
     });
-    it('should break text in lines if it does not fit', () => {
+
+    it('should break text in lines it does not fit', () => {
         expect(textWrapper("hello", 2)).toBe("he\nllo");
     });
+
     it('should break text if there is a space', () => {
         expect(textWrapper("hello world", 7)).toBe("hello\nworld");
     });
+
+
+    it('should break text if there are two spaces', () => {
+        expect(textWrapper("hello world mates", 7)).toBe("hello\nworld\nmates");
+    });
+
 });
