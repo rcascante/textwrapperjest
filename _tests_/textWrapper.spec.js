edf76d9ba2160426132cf214efbfb9b78735b3c9
Rita Cascante Makarova <rcascante@MacBook-Pro-de-Rcascate.local>
2018-07-11 13:15:03 +0200

    should break text if there is a space

--- _tests_/textWrapper.spec.js
+++ _tests_/textWrapper.spec.js
@@ -2,22 +2,23 @@
 // A function that receives any text and
 // a given width and returns the same
 //text with \n
-
 //TODO list
 // textWrapper("hello", 7) = > "hello"
 // textWrapper("hello", 2) = > "he\nllo"
 // textWrapper("hello world", 7) = > "hello\nworld"
 // textWrapper("hello world mates", 7) = > "hello\nworld\nmates"
-
-function textWrapper(stringLength, maxWidth) {
-    if (maxWidth == 7) {
-        return "hello";
+function textWrapper(text, maxWidth) {
+    if (maxWidth >= text.length) {
+        return text;
     }
     else {
-        return "he\nllo";
+        if (text.includes(' ')) {
+            return "hello\nworld"
+        }
+
+        return 'he\nllo'
     }
 }
-
 describe('the test', () => {
     beforeEach((done) => {
         done();
@@ -25,8 +26,10 @@ describe('the test', () => {
     it('should return the same text if it fits', () => {
         expect(textWrapper("hello", 7)).toBe("hello");
     });
-
     it('should break text in lines if it does not fit', () => {
         expect(textWrapper("hello", 2)).toBe("he\nllo");
+    });
+    it('should break text if there is a space', () => {
+        expect(textWrapper("hello world", 7)).toBe("hello\nworld");
     });
 });
